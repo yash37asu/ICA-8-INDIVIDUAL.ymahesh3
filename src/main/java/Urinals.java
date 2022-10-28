9c7d280311cceb646c1c074f3f522b613106fbf1
yash37asu <ymahesh3@asu.edu>
2022-10-28 00:20:55 -0700

    Main function called, all test cases working succesfully!

--- src/main/java/Urinals.java
+++ src/main/java/Urinals.java
@@ -1,47 +1,102 @@
 /*
 @author: Yashkumar Maheshwari
  */
+
 import java.io.*;
 import java.util.*;
 
 public class Urinals {
     ArrayList<String> inputs = new ArrayList<>();
-    public static void main(String[] args){
 
+    public static void main(String[] args) throws IOException {
+        String path = "urinal.dat";
+        Urinals urinal = new Urinals();
+        Boolean fileOpened = urinal.openFile(path);
+        System.out.println("file openend: urinal.dat \n input Arraylist generated!");
+        ArrayList<Integer> Results = new ArrayList<>();
+        Results = urinal.freeUrinals(urinal.inputs);
+        System.out.println("Inputs validated \n output arraylist generated!");
+        Boolean ruleCreated = urinal.writeToFile(Results);
+        System.out.println("Output file generated at : rule.txt ");
     }
+
     public Boolean openFile(String path) {
         try {
             File file = new File(path);
             BufferedReader br = new BufferedReader(new FileReader(file));
             String data;
-            while ((data = br.readLine()) != null){
-                if(data.equals("EOF") || data.equals("-1")) {
+            while ((data = br.readLine()) != null) {
+                if (data.equals("EOF") || data.equals("-1")) {
                     break;
                 }
                 inputs.add(data);
             }
             br.close();
             return true;
-        }
-        catch (FileNotFoundException E){
+        } catch (FileNotFoundException E) {
             return false;
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    public Boolean goodString( String str ) { // checks to see if valid string
+    public Boolean goodString(String str) { // checks to see if valid string
         return str != null;
     }
 
-    public Boolean validInput( String str){ // check if input is in 0 and 1 format
+    public Boolean validInput(String str) { // check if input is in 0 and 1 format
         boolean flag = true;
-        for(int i = 0;i<str.length();i++){
-            if (str.charAt(i) != '0' && str.charAt(i) != '1'){
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) != '0' && str.charAt(i) != '1') {
                 flag = false;
             }
         }
         return flag;
+    }
+
+    public ArrayList<Integer> freeUrinals(ArrayList<String> input) {
+        ArrayList<Integer> results = new ArrayList<>();
+        Urinals urinal = new Urinals();
+        for (int i = 0; i < input.size(); i++) {
+            String str = input.get(i);
+            int count = 0;
+            int n = str.length();
+            if (urinal.goodString(str) == false || urinal.validInput(str) == false) {
+                count = -1;
+                continue;
+            }
+            if (str.charAt(0) == '0' && str.charAt(1) == '0') {
+                count++;
+                StringBuffer chr = new StringBuffer(str);
+                chr.setCharAt(0, '1');
+                str = String.valueOf(chr);
+            }
+
+            for (int j = 1; j < n - 1; j++) {
+                if (str.charAt(j - 1) == '0' && str.charAt(j + 1) == '0' && str.charAt(j) == '0') {
+                    count++;
+                    StringBuffer chr = new StringBuffer(str);
+                    chr.setCharAt(i, '1');
+                    str = String.valueOf(chr);
+                }
+            }
+            for (int z = 0; z < n - 1; z++) {
+                if (n > 1) {
+                    if (str.charAt(z) == '1' && str.charAt(z + 1) == '1') {
+                        count = -1;
+                    }
+                }
+            }
+            if (str.charAt(n - 1) == '0' && str.charAt(n - 2) == '0') {
+                count++;
+                StringBuffer chr = new StringBuffer(str);
+                chr.setCharAt(n - 1, '1');
+                str = String.valueOf(chr);
+            }
+            results.add(count);
+
+        }
+        return results;
     }
 
     public Boolean writeToFile(ArrayList<Integer> results) throws IOException {
@@ -65,20 +120,7 @@ public class Urinals {
                 addresult.close();
             }
             return true;
-/*
-static public void writeOutputToFile(File file, int[] output) throws IOException {
-        BufferedWriter fileWriter = new BufferedWriter(new FileWriter(file));
-        for (int num: output) {
-            fileWriter.write(num + "");
-            fileWriter.newLine();
-        }
-        fileWriter.close();
-    }
-
-}
- */
-        }
-        catch (IOException E){
+        } catch (IOException E) {
             return false;
         }
     }
--- src/test/java/UrinalsTest.java
+++ src/test/java/UrinalsTest.java
@@ -1,44 +1,55 @@
 import org.junit.jupiter.api.Test;
+
 import java.util.*;
 import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 /**
-* urinals Tester. 
-* 
-* @author <Authors name> 
-* @since <pre>Oct 27, 2022</pre> 
-* @version 1.0 
-*/ 
+ * urinals Tester.
+ *
+ * @author <Authors name>
+ * @version 1.0
+ * @since <pre>Oct 27, 2022</pre>
+ */
 public class UrinalsTest {
-    Urinals urinal= new Urinals();
+    Urinals urinal = new Urinals();
 
     @Test
     void test_goodString() {
         String x = "yash";
-        assertEquals(true,urinal.goodString(x));
         System.out.println("====== Yashkumar Maheshwari == TEST ONE EXECUTED =======");
+        assertEquals(true, urinal.goodString(x));
     }
 
 
     @Test
     void validInput() {
         String x = "008701";
-        assertEquals(false,urinal.validInput(x));
         System.out.println("====== Yashkumar Maheshwari == TEST TWO EXECUTED =======");
+        assertEquals(false, urinal.validInput(x));
     }
 
     @Test
     void openFile() throws IOException {
         String path = "src/Urinal.dat";
-        assertEquals(true,urinal.openFile(path));
         System.out.println("====== Yashkumar Maheshwari == TEST THREE EXECUTED =======");
+        assertEquals(true, urinal.openFile(path));
+    }
+
+    @Test
+    void freeUrinals() {
+        ArrayList<String> input = new ArrayList<>(Arrays.asList("1010", "0100011", "0010001010010001"));
+        ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(0, -1, 3));
+        System.out.println("====== Yashkumar Maheshwari == TEST FOUR EXECUTED =======");
+        assertEquals(numbers, urinal.freeUrinals(input));
     }
 
     @Test
     void writetofile() throws IOException {
         ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6));
-        assertEquals(true,urinal.writeToFile(numbers));
+        System.out.println("====== Yashkumar Maheshwari == TEST FIVE EXECUTED =======");
+        assertEquals(true, urinal.writeToFile(numbers));
 
     }
 }
